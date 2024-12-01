cbde69110b5304218599822944fc813adc1780af
AriyanMlzm <ryanmolazem@gmail.com>
2024-12-01 18:43:34 +0330

    update pwa images

--- vite.config.ts
+++ vite.config.ts
@@ -17,25 +17,25 @@ export default defineConfig({
 				theme_color: '#FE5702',
 				icons: [
 					{
-						src: '/pwa-192x192.png',
+						src: '/movotopia-pwa-192x192.png',
 						sizes: '192x192',
 						type: 'image/png',
 						purpose: 'any',
 					},
 					{
-						src: '/pwa-512x512.png',
+						src: '/movotopia-pwa-512x512.png',
 						sizes: '512x512',
 						type: 'image/png',
 						purpose: 'any',
 					},
 					{
-						src: '/pwa-maskable-192x192.png',
+						src: '/movotopia-pwa-maskable-192x192.png',
 						sizes: '192x192',
 						type: 'image/png',
 						purpose: 'maskable',
 					},
 					{
-						src: '/pwa-maskable-512x512.png',
+						src: '/movotopia-pwa-maskable-512x512.png',
 						sizes: '512x512',
 						type: 'image/png',
 						purpose: 'maskable',
