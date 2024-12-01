7b6c384dc8fea44259e738db515d38b97270be65
AriyanMlzm <ryanmolazem@gmail.com>
2024-12-01 18:19:09 +0330

    update pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,48 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-	plugins: [svelte()],
+	plugins: [
+		svelte(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			manifest: {
+				short_name: 'Movotopia',
+				name: 'Movotopia',
+				start_url: '/svelte-moviesApi-tmdb',
+				display: 'standalone',
+				background_color: '#222222',
+				description: 'Search and list the Movies and Tv-shows.',
+				theme_color: '#FE5702',
+				icons: [
+					{
+						src: '/pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+						purpose: 'any',
+					},
+					{
+						src: '/pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any',
+					},
+					{
+						src: '/pwa-maskable-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+						purpose: 'maskable',
+					},
+					{
+						src: '/pwa-maskable-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'maskable',
+					},
+				],
+			},
+		}),
+	],
 	base: '/svelte-moviesApi-tmdb/',
 })
