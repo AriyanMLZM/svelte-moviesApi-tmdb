0617ff40386ac22c57196681b8e2b3d3129bdf1c
AriyanMlzm <ryanmolazem@gmail.com>
2025-05-10 16:27:57 +0330

    fix footer

--- src/App.svelte
+++ src/App.svelte
@@ -26,11 +26,6 @@
 <Navbar />
 <main class="w-full h-screen overflow-y-auto list-scroll">
 	<Router {routes} />
-	<footer class="w-full bg-black/50 text-white text-[0.8rem] mt-[60px] h-[80px] flex-center">
-		<a href="http://github.com/AriyanMlzm" target="_blank">
-			Developed by @AriyanMlzm
-		</a>
-	</footer>
 </main>
 
 <style>
